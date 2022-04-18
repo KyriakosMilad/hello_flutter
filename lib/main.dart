4cdc0ccfee6439931e6f51ed1b75bf60d33dd312
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-04-18 15:29:38 +0200

    Create question and answers

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,29 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('hello_flutter'),
         ),
-        body: Text('hi from body'),
+        body: Column(
+          children: [
+            Text('Question: Who is the founder of modern Egypt?'),
+            ElevatedButton(
+              child: Text('Al Sisi'),
+              onPressed: () {
+                print('alsisi');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Narmer'),
+              onPressed: () {
+                print('narmer');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Ramases II'),
+              onPressed: () {
+                print('ramasesii');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
