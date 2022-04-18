45920862bf2aa03e1878a73c74457f2a25e7486b
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-04-18 15:08:04 +0200

    Format file

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('hello_flutter'),),));
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('hello_flutter'),
+      ),
+    ));
   }
 }
